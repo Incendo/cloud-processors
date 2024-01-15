fa8f6ac1698890a3ccb8d9882022368a76ef0eee
Alexander Söderberg <alexander.soderberg@incendo.org>
2024-01-15 07:44:04 +0100

    build: use cloud snapshot, add deployment & configure renovate

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,6 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        mavenLocal()
         mavenCentral()
         maven("https://oss.sonatype.org/content/repositories/snapshots/") {
             name = "sonatypeOssSnapshots"
